fef35282859759eef485c1fafdb0b7f967064d28
boopalanv <boopalan457@gmail.com>
2024-05-01 18:44:48 +0900

    query function added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,33 @@
+
 fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Program arguments: ${args.joinToString()}")
+    //input arguments
+    val input = "param1=42&param2=54&param3=ololo&param3=trololo&param4=5&param5"
+    //object creation
+    val queryParamsImpl = QueryParamsImpl()
+    queryParamsImpl.execute(input)
+}
+
+internal interface QueryParams {
+    fun execute(query: String) : Map<String, List<String>>
+}
+
+class QueryParamsImpl : QueryParams {
+    override fun execute(query: String): Map<String, List<String>> {
+        val dat = query
+            .split("&")
+            .mapNotNull { keyAndValue ->
+                val keyAndValueList = keyAndValue.split("=")
+                keyAndValueList.takeIf { it.size == 2 }
+            }
+            .mapNotNull { keyAndValueList ->
+                val (key, value) = keyAndValueList
+                (key to value.trim()).takeIf { value.isNotBlank() }
+            }
+            .groupBy { (key, _) -> key }
+            .map { mapEntry ->
+                mapEntry.key to mapEntry.value.map { it.second }
+            }
+            .toMap()
+        return dat;
+    }
 }
